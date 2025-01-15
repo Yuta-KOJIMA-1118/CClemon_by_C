b676cf522a947add63d6be38d8cb13886e79aaf9
Yuta-KOJIMA-1118 <yutakojima1118@gmail.com>
2025-01-15 20:50:49 +0900

    1_15

---
+++ client/client.c
@@ -0,0 +1,151 @@
+// clientはWindows上で動く
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h> // キーボードからEnter無しでの入力用
+
+#include <winsock2.h> // Windows用ソケット リンクには -lws2_32 が必要
+
+WSADATA initialize();
+void finalize();
+
+void output_home();
+void output_with_friend_menu();
+
+void handle_with_friend_menu();
+
+int request_room_making();
+void request_room_searching(int n);
+
+int prepare_socket();
+
+int main() { // start client
+    initialize();
+    output_home();
+    while(1) {
+        if(_kbhit()) {
+            char c = _getch();
+            if(c == '1') {
+                printf("random matching\n");
+                break; //todo remove
+            } else if(c == '2') {
+                printf("matching with friend\n");
+                output_with_friend_menu();
+                handle_with_friend_menu();
+                break; //todo remove
+            } else if(c == '3') {
+                printf("matching with npc\n");
+                break; //todo remove
+            } else if(c == '4') {
+                printf("exit\n");
+                finalize();
+            }
+            else {
+                printf("invalid input\n");
+                printf("please type 1, 2, 3, 4\n");
+            }
+        }
+    }
+    finalize();
+}
+
+WSADATA initialize() {
+    WSADATA wsaData;
+    if(WSAStartup(MAKEWORD(2, 0), &wsaData) != 0) {
+        perror("WSAStartup");
+        exit(1);
+    }
+    system("cls");
+    return wsaData;
+}
+
+void finalize() {
+    WSACleanup();
+    exit(0);
+}
+
+
+void output_home() {
+    system("cls");
+    printf("CClemon online\n");
+    printf("\n");
+    printf("select menu\n");
+    printf("1. random matching\n");
+    printf("2. matching with friend\n");
+    printf("3. mathcing with npc\n");
+    printf("4. exit\n");
+    printf("\n");
+}
+
+void output_with_friend_menu() {
+    printf("type 0 to room making\n");
+    printf("type negative number to exit\n");
+    printf("other number to room entering\n");
+    printf("\n");
+}
+
+void handle_with_friend_menu() {
+    int n;
+    scanf("%d", &n);
+    printf("n = %d\n", n);
+    int room_id;
+    if(n == 0) {
+        printf("room making\n");
+        room_id = request_room_making();
+    }
+    else if(n < 0) {
+        printf("exit\n");
+        finalize();
+    }
+    else {
+        printf("searching %d room\n", n);
+        room_id = n;
+        request_room_searching(n);
+    }
+}
+
+int request_room_making() {
+    int sockfd = prepare_socket();
+    char *buf = "room_making 0\0";
+    int len;
+    send(sockfd, buf, strlen(buf), 0);
+    len = recv(sockfd, buf, len, 0);
+    buf[len] = '\0';
+    closesocket(sockfd);
+    int room_id = atoi(buf);
+    return room_id;
+}
+
+void request_room_searching(int n) {
+
+}
+
+int prepare_socket() {
+    SOCKET sockfd;
+    struct sockaddr_in serv;
+    const char *ip = "127.0.0.1";
+    int port = 12345;
+    
+    // ソケットの作成
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == INVALID_SOCKET) {
+        perror("socket");
+        exit(1);
+    }
+
+    // サーバー情報の設定
+    memset(&serv, 0, sizeof(serv));
+    serv.sin_family = AF_INET;
+    serv.sin_port = htons(port);
+    serv.sin_addr.s_addr = inet_addr(ip);
+
+    // サーバーに接続
+    if (connect(sockfd, (struct sockaddr *)&serv, sizeof(serv)) == SOCKET_ERROR) {
+        perror("connect");
+        closesocket(sockfd);
+        exit(1);
+    }
+
+    return sockfd;
+}
+
---
+++ server/server.c
@@ -0,0 +1,327 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#include <sys/shm.h> // 共有メモリ
+
+#define NUM_OF_ROOM 1000
+
+typedef enum RoomState {
+    EMPTY,
+    WAITING,
+    PLAYING
+}RoomState;
+
+typedef enum SkillType {
+    CHARGE,
+    ATTACK,
+    DEFENCE,
+    REFLECT
+}SkillType;
+
+typedef struct Skill {
+    char name[10];
+    int priority;
+    int energy;
+    SkillType type;
+}Skill;
+
+typedef struct Player {
+    char name[20];
+    Skill next_skill;
+    int sockfd;
+}Player;
+
+typedef struct Room {
+    RoomState state;
+    Player players[2];
+}Room;
+
+typedef enum SkillName {
+    LEMON,
+    FIRE,
+    BARRIER,
+    CHANGE,
+    GUN
+}SkillName;
+
+void prepare_skills();
+void prepare_mutexes();
+void destroy_mutexes();
+void prepare_shared_memory(int *shm_id, Room **rooms);
+void init_room(Room *room);
+Room *attach_rooms(int shm_id);
+void detach_rooms(Room *rooms);
+Room get_room_and_lock(Room *rooms, int room_id);
+void unlock_room(int room_id);
+int prepare_socket(socklen_t *sin_siz);
+void reciever(int new_sockfd, int shm_id);
+int room_making(int shm_id);
+int room_searching(int room_id);
+
+// グローバル
+Skill skills[5];
+pthread_mutex_t room_mutexes[NUM_OF_ROOM];
+
+int main() {
+    prepare_skills();
+
+    int shm_id;
+    Room *rooms;
+    prepare_shared_memory(&shm_id, &rooms);
+
+    int sockfd;
+    socklen_t sin_siz;
+    sockfd = prepare_socket(&sin_siz);
+
+    while(1) {
+        int new_sockfd;
+        struct sockaddr_in clnt;
+        if((new_sockfd = accept(sockfd, (struct sockaddr *)&clnt, &sin_siz)) < 0) {
+            perror("accept");
+            continue;
+        }
+
+        pid_t pid = fork();
+        switch(pid) {
+            case -1:
+                perror("fork");
+                break;
+
+            case 0: // 子プロセス
+                printf("connect from %s: %d\n", inet_ntoa(clnt.sin_addr), ntohs(clnt.sin_port));
+                reciever(new_sockfd, shm_id);
+                close(new_sockfd);
+                exit(0);
+
+            default: // 親プロセス
+                close(new_sockfd);
+                break;
+        }
+    }
+    close(sockfd);
+    shmdt(rooms);
+    shmctl(shm_id, IPC_RMID, NULL);
+    destroy_mutexes();
+    return 0;
+}
+
+void prepare_mutexes() {
+    for(int i=0; i<NUM_OF_ROOM; i++) {
+        if(pthread_mutex_init(&room_mutexes[i], NULL) != 0) {
+            perror("pthread_mutex_init");
+            exit(1);
+        }
+    }
+}
+
+void destroy_mutexes() {
+    for(int i=0; i<NUM_OF_ROOM; i++) {
+        if(pthread_mutex_destroy(&room_mutexes[i]) != 0) {
+            perror("pthread_mutex_destroy");
+            exit(1);
+        }
+    }
+}
+
+void prepare_skills() {
+    strcpy(skills[0].name, "lemon");
+    skills[0].priority = 0;
+    skills[0].energy = -1;
+    skills[0].type = CHARGE;
+
+    strcpy(skills[1].name, "fire");
+    skills[1].priority = 1;
+    skills[1].energy = 3;
+    skills[1].type = ATTACK;
+
+    strcpy(skills[2].name, "barrier");
+    skills[2].priority = 2;
+    skills[2].energy = 0;
+    skills[2].type = DEFENCE;
+
+    strcpy(skills[3].name, "change");
+    skills[3].priority = 3;
+    skills[3].energy = 1;
+    skills[3].type = REFLECT;
+
+    strcpy(skills[4].name, "gun");
+    skills[4].priority = 4;
+    skills[4].energy = 5;
+    skills[4].type = ATTACK;
+}
+
+void prepare_shared_memory(int *shm_id, Room **rooms) {
+    if((*shm_id = shmget(IPC_PRIVATE, sizeof(Room) * NUM_OF_ROOM, IPC_CREAT | 0666)) < 0) {
+        perror("shmget");
+        exit(1);
+    }
+    if((*rooms = (Room *)shmat(*shm_id, 0, 0)) == (void *)-1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    for(int i=0; i<NUM_OF_ROOM; i++) {
+        init_room(&(*rooms)[i]);
+    }
+}
+
+void init_room(Room *room) {
+    room->state = EMPTY;
+    for(int i=0; i<2; i++) {
+        room->players[i].name[0] = '\0';
+        room->players[i].next_skill.name[0] = '\0';
+        room->players[i].next_skill = skills[0]; // lemon
+        room->players[i].sockfd = -1;
+    }
+}
+
+Room *attach_rooms(int shm_id) {
+    Room *rooms;
+    if((rooms = (Room *)shmat(shm_id, NULL, 0)) == (void *)-1) {
+        perror("shmat");
+        exit(1);
+    }
+    return rooms;
+}
+
+void detach_rooms(Room *rooms) {
+    if(shmdt(rooms) == -1) {
+        perror("shmdt");
+        exit(1);
+    }
+}
+
+Room get_room_and_lock(Room *rooms, int room_id) {
+    if (room_id < 0 || room_id >= NUM_OF_ROOMS) {
+//todo 終了処理
+        fprintf(stderr, "Invalid room_id: %d\n", room_id);
+        exit(1);
+    }
+
+    // ロックが取得できるまで待機
+    while (1) {
+        int lock_status = pthread_mutex_lock(&room_mutexes[room_id]);
+
+        if (lock_status == 0) {
+            break;
+        } else if (lock_status == EINVAL) {
+//todo 終了処理
+            fprintf(stderr, "Mutex is not initialized for room_id: %d\n", room_id);
+            exit(1);
+        } else if (lock_status == EDEADLK) {
+            fprintf(stderr, "Deadlock detected for room_id: %d\n", room_id);
+            exit(1);
+        } else {
+            perror("pthread_mutex_lock");
+            exit(1);
+        }
+    }
+
+    // 該当する Room を返す
+    return rooms[room_id];
+}
+
+
+void unlock_room(int room_id) {
+    if (room_id < 0 || room_id >= NUM_OF_ROOMS) {
+//todo 終了処理
+        fprintf(stderr, "Invalid room_id: %d\n", room_id);
+        exit(1);
+    }
+
+    if (pthread_mutex_unlock(&room_mutexes[room_id]) != 0) {
+        perror("pthread_mutex_unlock");
+        exit(1);
+    }
+}
+int prepare_socket(socklen_t *sin_siz) {
+    int sockfd;
+    struct sockaddr_in serv;
+    char *ip = "127.0.0.1";
+    int port = 12345;
+    
+    if((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+    printf("socket() called\n");
+
+    serv.sin_family = PF_INET;
+    serv.sin_port = htons(port);
+    inet_aton(ip, &serv.sin_addr);
+    *sin_siz = sizeof(struct sockaddr_in);
+    if(bind(sockfd, (struct sockaddr *)&serv, sizeof(serv)) < 0) {
+        perror("bind");
+        exit(1);
+    }
+    printf("bind() called\n");
+
+    if(listen(sockfd, SOMAXCONN) < 0) {
+        perror("listen");
+        exit(1);
+    }
+    printf("listen() called\n");
+
+    return sockfd;
+}
+
+void reciever(int new_sockfd, int shm_id) {
+    char buf[100];
+    memset(buf, 0, sizeof(buf));
+    int len = recv(new_sockfd, buf, BUFSIZ, 0);
+    printf("len: %d\n", len);
+    buf[len] = '\0';
+    printf("received: %s\n", buf);
+
+    // buf: [label] [data]
+    char *label = strtok(buf, " ");
+    char *data = strtok(NULL, " ");
+
+    printf("label: %s\n", label);
+    printf("data: %s\n", data);
+
+    if(strcmp(label, "room_making") == 0) {
+        int room_id = room_making(shm_id);
+        printf("room_id: %d\n", room_id);
+        send(new_sockfd, buf, strlen(buf), 0);
+    }
+    else if(strcmp(label, "room_searching") == 0) {
+        int room_id = atoi(data);
+        room_searching(room_id);
+    }
+    else {
+        printf("unknown label\n");
+    }
+}
+
+int room_making(int shm_id) {
+    printf("room_making\n");
+
+    Room *rooms = attach_rooms(shm_id);
+    int room_id;
+
+    for (int i = 0; i < NUM_OF_ROOM; i++) {
+        if (rooms[i].state == EMPTY) {
+            rooms[i].state = WAITING;
+            room_id = i;
+            break;
+        }
+    }
+
+    detach_rooms(rooms);
+    return room_id;
+}
+
+int room_searching(int room_id) {
+    printf("room_searching %d\n", room_id);
+    return 1;
+}
+
+
